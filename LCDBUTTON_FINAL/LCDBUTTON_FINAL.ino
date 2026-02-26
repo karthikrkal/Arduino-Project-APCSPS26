7bd9e4beef532f806e166f738ec803c96f9029e2
karthikrkal <karthik.r.kalyan@gmail.com>
2026-02-26 11:32:40 -0800

    day 1 push - add config to 2 leds, buttons, lcd, logic fwk pending

--- LCDBUTTON_FINAL/LCDBUTTON_FINAL.ino
+++ LCDBUTTON_FINAL/LCDBUTTON_FINAL.ino
@@ -5,15 +5,16 @@ LiquidCrystal lcd(53,52,51,50,49,48);
 //variables
 const int switchNum = 13;
 int on = digitalRead(switchNum);
-int blueButPin = 12;
-int redButPin = 11;
-int greenButPin = 12;
-int blueBut = digitalRead(blueButPin);
-int redBut = digitalRead(redButPin);
-int greenBut = digitalRead(greenButPin);
-bool blueHit = false;
-bool redHit = false;
-bool greenHit = false;
+int bluePin = 12;
+int redPin = 11;
+int greenPin = 12;
+int speakerPin = 9;
+int led1 = 9;
+int led2 = 8;
+int blueButton = digitalRead(bluePin);
+int redButton = digitalRead(redPin);
+int greenButton = digitalRead(greenPin);
+
 int score = 0;
 
 void setup() {
@@ -25,9 +26,12 @@ void setup() {
   Serial.begin(9600);
 
   //pins
-  pinMode(blueButPin,INPUT);
-  pinMode(redButPin,INPUT);
-  pinMode(greenButPin,INPUT);
+  pinMode(bluePin,INPUT);
+  pinMode(redPin,INPUT);
+  pinMode(greenPin,INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(speakerPin, OUTPUT);
   
 }
 
@@ -35,27 +39,31 @@ void loop() {
 
   //Variable Updates
   on = digitalRead(switchNum);
-  blueBut = digitalRead(blueButPin);
-  redBut = digitalRead(redButPin);
-  greenBut = digitalRead(greenButPin);
+  blueButton = digitalRead(bluePin);
+  redButton = digitalRead(redPin);
+  greenButton = digitalRead(greenPin);
+  pinMode(speakerPin, OUTPUT);
 
   //Serial
-  Serial.print("blue: " + String(blueBut));
-  Serial.print("red: " + String(redBut));
-  Serial.print("green: " + String(greenBut));
-  Serial.print(blueHit);
+  Serial.print("blue: " + String(blueButton));
+  Serial.print("red: " + String(redButton));
+  Serial.print("green: " + String(greenButton));
+  Serial.print(blueButton);
+
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
 
   // //hit settings
-  // if(blueButPin == 1){
+  // if(bluePin == 1){
   //   blueHit == true;
   // }
-  // if(redButPin == 1){
+  // if(redPin == 1){
   //   redHit == true;
   // }
-  // if(greenButPin == 1){
+  // if(greenPin == 1){
   //   greenHit == true;
   // }
-  
+
 
 
   //Bulk Code
